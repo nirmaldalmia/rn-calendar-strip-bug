e4f1b47a106d5d39664bdcaa53f8cd183c7f6e97
nirmaldalmia <nirmaldalmia17@gmail.com>
2020-06-22 20:43:44 +0530

    add calendar strip

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,6 +3,7 @@ import { View, Text, Button } from "react-native";
 import { inject, observer } from "mobx-react";
 import { _storeData } from "storage";
 import RNLogo from "assets/img/rn_logo.svg";
+import CalendarStrip from "react-native-calendar-strip";
 
 @inject("counterStore")
 @observer
@@ -24,6 +25,25 @@ export default class Home extends React.Component {
         <Button
           title="Go to About"
           onPress={() => this.props.navigation.navigate("About")}
+        />
+        <CalendarStrip
+          ref={"calendarStrip"}
+          style={{ height: 90 }}
+          scrollable={true}
+          useIsoWeekday={true}
+          //startingDate={new Date()}
+          selectedDate={new Date()}
+          onDateSelected={(date) => console.log("Selected date is: ", date)}
+          updateWeek={true}
+          markedDatesStyle={{ borderColor: "#000" }}
+          daySelectionAnimation={{
+            type: "background",
+            highlightColor: "blue",
+          }}
+          highlightDateNameStyle={{
+            color: "#fff",
+          }}
+          highlightDateNumberStyle={{ color: "#fff" }}
         />
       </View>
     );
